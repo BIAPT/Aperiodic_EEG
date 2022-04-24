d4c2de518dfed47a4650477c0e285499170c6d8a
François Paugam <35327799+FrancoisPgm@users.noreply.github.com>
2022-04-24 01:35:10 -0400

    apply black linter and use f strings

--- _1_spectral_decomposition.py
+++ _1_spectral_decomposition.py
@@ -12,17 +12,27 @@ import mne
 import os
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Calculates the Power spectral density.')
-    parser.add_argument('input_dir', type=str, action='store',
-                        help='folder name containing the data in epoched .fif data in BIDS format')
-    parser.add_argument('output_dir', type=str, action='store',
-                        help='folder name where to save the power spectra')
-    parser.add_argument('patient_information', type=str, action='store',
-                        help='path to txt with information about participants')
-    parser.add_argument('condition', type=str, action='store',
-                        help='The "task" or condition you want to analyze')
+    parser = argparse.ArgumentParser(description="Calculates the Power spectral density.")
+    parser.add_argument(
+        "input_dir",
+        type=str,
+        action="store",
+        help="folder name containing the data in epoched .fif data in BIDS format",
+    )
+    parser.add_argument(
+        "output_dir", type=str, action="store", help="folder name where to save the power spectra"
+    )
+    parser.add_argument(
+        "patient_information",
+        type=str,
+        action="store",
+        help="path to txt with information about participants",
+    )
+    parser.add_argument(
+        "condition", type=str, action="store", help='The "task" or condition you want to analyze'
+    )
     args = parser.parse_args()
 
     """
@@ -31,35 +41,31 @@ if __name__ == '__main__':
 
     # make ouput directory
     output_dir = os.path.join(args.output_dir, args.condition)
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
 
     # prepare output pdf
-    pdf = pltpdf.PdfPages("{}/spectral_decomposition_{}.pdf".format(output_dir, args.condition))
+    pdf = pltpdf.PdfPages(f"{output_dir}/spectral_decomposition_{args.condition}.pdf")
 
     # load patient IDS
-    info = pd.read_csv(args.patient_information, sep='\t')
-    P_IDS = info['Patient']
+    info = pd.read_csv(args.patient_information, sep="\t")
+    P_IDS = info["Patient"]
 
-    # define empty DataFrames to save
-    # 1) Welch Power density
-    power_spectra_welch = []
-    # 2) Multitaper power density
-    power_spectra_mt = []
+    # define empty lists to save
+    power_spectra_welch = []  # 1) Welch Power density
+    power_spectra_mt = []  #    2) Multitaper power density
 
     for p_id in P_IDS:
         """
-            2)      IMPORT DATA
+        2)      IMPORT DATA
         """
         # define epoch name
-        input_fname =  "{}/sub-{}/eeg/epochs_{}_{}.fif".format(args.input_dir, p_id, p_id, args.condition)
+        input_fname = f"{args.input_dir}/sub-{p_id}/eeg/epochs_{p_id}_{args.condition}.fif"
         raw_epochs = mne.read_epochs(input_fname)
 
         # remove channels marked as bad and non-brain channels
-        raw_epochs.drop_channels(raw_epochs.info['bads'])
+        raw_epochs.drop_channels(raw_epochs.info["bads"])
 
-        #crop data if necessary
+        # crop data if necessary
         if len(raw_epochs) > 30:
             epochs_cropped = raw_epochs[-30:]
         else:
@@ -82,10 +88,15 @@ if __name__ == '__main__':
         # Hanning window of 2 s
         # 1 s overlap
         sampling_rate = 250
-        psds_welch, freqs_welch = psd_welch(epochs_cropped, fmin=0.5, fmax=50,
-                                            n_fft=2*sampling_rate, n_per_seg=2*sampling_rate,
-                                            n_overlap=sampling_rate)
-        psds_welch_db = 10 * np.log10(psds_welch) # convert to dB
+        psds_welch, freqs_welch = psd_welch(
+            epochs_cropped,
+            fmin=0.5,
+            fmax=50,
+            n_fft=2 * sampling_rate,
+            n_per_seg=2 * sampling_rate,
+            n_overlap=sampling_rate,
+        )
+        psds_welch_db = 10 * np.log10(psds_welch)  # convert to dB
 
         # save the PSD for later analysis
         power_spectra_welch.append(psds_welch)
@@ -100,7 +111,7 @@ if __name__ == '__main__':
         # See this paper: https://journals.physiology.org/doi/full/10.1152/physiol.00062.2015
 
         psds_mt, freqs_mt = psd_multitaper(epochs_cropped, fmin=0.5, fmax=50, bandwidth=1)
-        psds_mt_db = 10 * np.log10(psds_mt) # convert to dB
+        psds_mt_db = 10 * np.log10(psds_mt)  # convert to dB
 
         # save the PSD for later analysis
         power_spectra_mt.append(psds_mt)
@@ -109,11 +120,19 @@ if __name__ == '__main__':
             6)    Plot PSD Welch and Multitaper
         """
         # plot results
-        fig = plot_two_curves(x1=freqs_welch, x2=freqs_mt,
-                              y1=psds_welch_db.mean(0), y2=psds_mt_db.mean(0),
-                              c1='green', c2='orange', l1='Welch', l2='Multitaper',
-                              title='{} Power spectral density'.format(p_id),
-                              lx='Frequency (Hz)', ly='Power Spectral Density (dB)')
+        fig = plot_two_curves(
+            x1=freqs_welch,
+            x2=freqs_mt,
+            y1=psds_welch_db.mean(0),
+            y2=psds_mt_db.mean(0),
+            c1="green",
+            c2="orange",
+            l1="Welch",
+            l2="Multitaper",
+            title=f"{p_id} Power spectral density",
+            lx="Frequency (Hz)",
+            ly="Power Spectral Density (dB)",
+        )
         pdf.savefig(fig)
         plt.close(fig)
         plt.show()
@@ -122,17 +141,25 @@ if __name__ == '__main__':
     power_spectra_mt.append(P_IDS)
     power_spectra_welch.append(P_IDS)
 
-    with open('{}/PSD_Welch_{}.pkl'.format(output_dir,args.condition), 'wb') as f:
+    with open(f"{output_dir}/PSD_Welch_{args.condition}.pkl", "wb") as f:
         pickle.dump(power_spectra_welch, f)
 
-    with open('{}/PSD_Multitaper_{}.pkl'.format(output_dir,args.condition), 'wb') as f:
+    with open(f"{output_dir}/PSD_Multitaper_{args.condition}.pkl", "wb") as f:
         pickle.dump(power_spectra_mt, f)
 
     freqs_mt = pd.DataFrame(freqs_mt)
     freqs_welch = pd.DataFrame(freqs_welch)
-    freqs_welch.to_csv('{}/Frequency_Welch_{}.txt'.format(output_dir, args.condition),
-                       index=None, header=None, sep=' ')
-    freqs_mt.to_csv('{}/Frequency_Multitaper_{}.txt'.format(output_dir, args.condition),
-                    index=None, header=None, sep=' ')
+    freqs_welch.to_csv(
+        "{}/Frequency_Welch_{}.txt".format(output_dir, args.condition),
+        index=None,
+        header=None,
+        sep=" ",
+    )
+    freqs_mt.to_csv(
+        "{}/Frequency_Multitaper_{}.txt".format(output_dir, args.condition),
+        index=None,
+        header=None,
+        sep=" ",
+    )
 
     pdf.close()
--- _2a_parametrization_tr.py
+++ _2a_parametrization_tr.py
@@ -8,103 +8,118 @@ import pickle
 import os
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Decomposes the signal using FOOOF in a time-resolved way')
-    parser.add_argument('input_dir', type=str, action='store',
-                        help='folder name containing the PSD pickles from step 1')
-    parser.add_argument('output_dir', type=str, action='store',
-                        help='directory for results to be saved')
-    parser.add_argument('data_dir', type=str, action='store',
-                        help='folder name containing the data in .fif format in BIDS format')
-    parser.add_argument('patient_information', type=str, action='store',
-                        help='path to txt with information about participants')
-    parser.add_argument('condition', type=str, action='store',
-                        help='The "task" or conditions you want to caluclate for example Baseline or Anesthesia')
-    parser.add_argument('--method', action='store', default='Multitaper', choices=('Multitaper','Welch'),
-                        help='The method used for Spectral decomposition in Step 1')
+    parser = argparse.ArgumentParser(
+        description="Decomposes the signal using FOOOF in a time-resolved way"
+    )
+    parser.add_argument(
+        "input_dir",
+        type=str,
+        action="store",
+        help="folder name containing the PSD pickles from step 1",
+    )
+    parser.add_argument(
+        "output_dir", type=str, action="store", help="directory for results to be saved"
+    )
+    parser.add_argument(
+        "data_dir",
+        type=str,
+        action="store",
+        help="folder name containing the data in .fif format in BIDS format",
+    )
+    parser.add_argument(
+        "patient_information",
+        type=str,
+        action="store",
+        help="path to txt with information about participants",
+    )
+    parser.add_argument(
+        "condition",
+        type=str,
+        action="store",
+        help='The "task" or conditions you want to caluclate for example Baseline or Anesthesia',
+    )
+    parser.add_argument(
+        "--method",
+        action="store",
+        default="Multitaper",
+        choices=("Multitaper", "Welch"),
+        help="The method used for Spectral decomposition in Step 1",
+    )
 
     # read out arguments
     args = parser.parse_args()
     method = args.method
     cond = args.condition
-    print(cond)
-    #
-    # output_dir = args.output_dir
-    #
-    # # prepare output dir
-    # if not os.path.exists(output_dir):
-    #     os.makedirs(output_dir)
-    #
-    # # load participant info
-    # info = pd.read_csv(args.patient_information, sep='\t')
-    # P_IDS = info['Patient']
-    #
-    # # create empty output
-    # slope_1_45_knee = []
-    # offset_1_45_knee = []
-    # error_knee = []
-    #
-    # # load power spectral data
-    # PSD = pickle.load(open("{}/{}/PSD_{}_{}.pkl".format(args.input_dir, cond, method, cond), "rb"))
-    # PSD_ID = PSD[-1]
-    #
-    # # load frequencies
-    # datapath = os.path.join(args.input_dir, cond, 'Frequency_{}_{}.txt'.format(method, cond))
-    # freqs = pd.read_csv(datapath, sep=' ', header=None)
-    # freqs = np.squeeze(freqs)
-    #
-    # for p_id in P_IDS:
-    #     # select individual PSD values, depending on ID
-    #     index_id = np.where(PSD_ID == p_id)[0][0]
-    #     PSD_p = PSD[index_id]
-    #
-    #     # load frequencies
-    #     datapath = os.path.join(args.input_dir, cond, 'Frequency_{}_{}.txt'.format(method, cond))
-    #     freqs = pd.read_csv(datapath, sep=' ', header=None)
-    #     freqs = np.squeeze(freqs)
-    #     freqs = np.array(freqs)
-    #
-    #     """
-    #     Run FOOOF model in 3D
-    #     """
-    #     # power spectra across data epochs within subjects, as [n_epochs, n_channels, n_freqs]
-    #     PSD_p_3d = np.array(PSD_p)
-    #
-    #     offset_tmp = []
-    #     exponent_tmp = []
-    #     error_tmp = []
-    #
-    #     # initiate FOOOF model:
-    #     fg = FOOOFGroup(aperiodic_mode='knee', min_peak_height=0.1, max_n_peaks=10)
-    #     # Fit the 3D array of power spectra
-    #     fgs = fit_fooof_3d(fg, np.array(freqs), PSD_p_3d, freq_range=[1,45], n_jobs=10)
-    #
-    #     # collect results
-    #     for ind, fg in enumerate(fgs):
-    #         # Aperiodic parameters averaged over space
-    #         exponent_tmp.append(np.nanmean(fg.get_params('aperiodic_params', 'exponent')))
-    #         offset_tmp.append(np.nanmean(fg.get_params('aperiodic_params', 'offset')))
-    #         error_tmp.append(np.nanmean(fg.get_params('error')))
-    #
-    #     slope_1_45_knee.append(np.mean(exponent_tmp))
-    #     offset_1_45_knee.append(np.mean(offset_tmp))
-    #     error_knee.append(np.mean(error_tmp))
-    #
-    #     # save data for one participant in a txt
-    #     params_p = []
-    #     params_p.append([p_id, np.mean(exponent_tmp), np.mean(offset_tmp),np.mean(error_tmp)])
-    #
-    #     params_time_df = pd.DataFrame(params_p, columns=['ID', 'slope_1_45_knee', 'offset_1_45_knee','error_1_45_knee'])
-    #
-    #     params_time_df.to_csv('{}/Params_time_{}_{}.txt'.format(output_dir, p_id, cond), index=False, sep=' ')
-    #     print("Finished Subject  {}".format(p_id))
-    #
-    # params_df = pd.DataFrame()
-    # params_df['ID'] = P_IDS
-    #
-    # params_df['slope_1_45_knee'] = slope_1_45_knee
-    # params_df['offset_1_45_knee'] = offset_1_45_knee
-    # params_df['error_knee'] = error_knee
-    #
-    # params_df.to_csv('{}/Parametrized_{}_TR.txt'.format(output_dir, cond), index=False, sep=' ')
+
+    output_dir = args.output_dir
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    # load participant info
+    info = pd.read_csv(args.patient_information, sep="\t")
+    P_IDS = info["Patient"]
+
+    # create empty output
+    slope_1_45_knee = []
+    offset_1_45_knee = []
+    error_knee = []
+
+    # load power spectral data
+    PSD = pickle.load(open(f"{args.input_dir}/{cond}/PSD_{method}_{cond}.pkl", "rb"))
+    PSD_ID = PSD[-1]
+
+    # load frequencies
+    datapath = os.path.join(args.input_dir, cond, f"Frequency_{method}_{cond}.txt")
+    freqs = pd.read_csv(datapath, sep=" ", header=None)
+    freqs = np.array(np.squeeze(freqs))
+
+    for p_id in P_IDS:
+        # select individual PSD values, depending on ID
+        index_id = np.where(PSD_ID == p_id)[0][0]
+        PSD_p = PSD[index_id]
+
+        """
+        Run FOOOF model in 3D
+        """
+        # power spectra across data epochs within subjects, as [n_epochs, n_channels, n_freqs]
+        PSD_p_3d = np.array(PSD_p)
+
+        offset_tmp = []
+        exponent_tmp = []
+        error_tmp = []
+
+        # initiate FOOOF model:
+        fg = FOOOFGroup(aperiodic_mode="knee", min_peak_height=0.1, max_n_peaks=10)
+        # Fit the 3D array of power spectra
+        fgs = fit_fooof_3d(fg, freqs, PSD_p_3d, freq_range=[1, 45], n_jobs=10)
+
+        # collect results
+        for fg in fgs:
+            # Aperiodic parameters averaged over space
+            exponent_tmp.append(np.nanmean(fg.get_params("aperiodic_params", "exponent")))
+            offset_tmp.append(np.nanmean(fg.get_params("aperiodic_params", "offset")))
+            error_tmp.append(np.nanmean(fg.get_params("error")))
+
+        slope_1_45_knee.append(np.mean(exponent_tmp))
+        offset_1_45_knee.append(np.mean(offset_tmp))
+        error_knee.append(np.mean(error_tmp))
+
+        # save data for one participant in a txt
+        params_p = [[p_id, np.mean(exponent_tmp), np.mean(offset_tmp), np.mean(error_tmp)]]
+
+        params_time_df = pd.DataFrame(
+            params_p, columns=["ID", "slope_1_45_knee", "offset_1_45_knee", "error_1_45_knee"]
+        )
+
+        params_time_df.to_csv(f"{output_dir}/Params_time_{p_id}_{cond}.txt", index=False, sep=" ")
+        print(f"Finished Subject  {p_id}")
+
+    params_df = pd.DataFrame()
+    params_df["ID"] = P_IDS
+
+    params_df["slope_1_45_knee"] = slope_1_45_knee
+    params_df["offset_1_45_knee"] = offset_1_45_knee
+    params_df["error_knee"] = error_knee
+
+    params_df.to_csv(f"{output_dir}/Parametrized_{cond}_TR.txt", index=False, sep=" ")
